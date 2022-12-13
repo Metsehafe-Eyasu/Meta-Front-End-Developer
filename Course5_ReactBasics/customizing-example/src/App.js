7b9287ef7380e8b9dddb43f0d950a62f6e3969b5
Metsehafe-Eyasu <metsehafe.eyasu@aastustudent.edu.et>
2022-12-13 04:48:33 +0300

    Started learning React js

---
+++ Course5_ReactBasics/customizing-example/src/App.js
@@ -0,0 +1,22 @@
+import Promo from './components/Promo'
+import Nav from './components/Nav'
+import Intro1 from './components/Intro1'
+import Intro2 from './components/Intro2'
+import Intro3 from './components/Intro3'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Promo />
+      <Nav />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Course5_ReactBasics/first-component/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+function App() {
+  return (
+    <div className="App">   
+      <Heading firstName="Bob"/>
+      <Heading firstName="Jeff"/>
+      <Heading firstName="Henry"/>
+    </div>
+  );
+}
+
+export default App;
